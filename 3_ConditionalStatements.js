7e413fafa77d423bbf9f938de58a7ed475a245d1
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-03 22:57:36 +0000

    js

---
+++ 3_ConditionalStatements.js
@@ -0,0 +1,29 @@
+// the conditional statements 
+
+// well there are the following:
+// if statement
+// the else if statement
+// the else statement
+// the switch statement
+
+
+a = 10.33 // put any number  (Can be integer or float) 
+// complex numbers dont work obvio :)
+
+// if statement
+if (a >= 10){
+    console.log ("the number is above 10 or is 10") // prints the output
+}
+
+// else if statement
+else if (5>a>10){
+    console.log ("the number is less than 10 but is greater than 5") // prints the output
+}
+
+// else statement 
+else {
+    console.log ("the number is probably 5 or less") // prints the output
+}
+
+
+// switch in the next one
